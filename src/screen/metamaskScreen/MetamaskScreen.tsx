1f6966ce5d7ddb4acbaf3aed503f117f6d1c8874
Federico Jure <fjure@atixlabs.com>
2021-07-21 20:42:56 -0300

    update link to metamask download page

--- src/screen/metamaskScreen/MetamaskScreen.tsx
+++ src/screen/metamaskScreen/MetamaskScreen.tsx
@@ -5,7 +5,7 @@ const MetamaskScreen = () => (
   <Message>
     <Message.Header>Ups!</Message.Header>
     <p>
-      Seems like you dont have metamask installed! You can switch to this <a href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn/related?hl=es">link</a> to install:
+      Seems like you dont have metamask installed! You can switch to this <a href="https://metamask.io/download.html">link</a> to install:
     </p>
   </Message>
 )
